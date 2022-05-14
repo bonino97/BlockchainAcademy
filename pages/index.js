17fa9ce961eb9820fa3cd6144c50e9458b80b9d4
bonino97 <juanbonino97@gmail.com>
2022-05-14 18:08:07 -0300

    Adding courses/design

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,26 @@ import { WalletBar, EthRates } from "@components/web3";
 import { CourseList } from "@components/course";
 import { OrderCard } from "@components/order";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/fetcher";
 
-const Home = () => {
-
+const Home = ({ courses }) => {
   return (
     <>
       <Hero />
-      <Breadcrumbs />
+      {/* <Breadcrumbs />
       <WalletBar />
       <EthRates />
-      <OrderCard />
-      <CourseList />
+      <OrderCard /> */}
+      <CourseList courses={courses} />
     </>
   );
+};
+
+export const getStaticProps = () => {
+  const { data } = getAllCourses();
+  return {
+    props: { courses: data },
+  };
 };
 
 Home.Layout = BaseLayout;
